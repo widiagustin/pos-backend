a34190e065326c343a6064dbd002b338151d0760
widiagustin <widhianiagustin14@gmail.com>
2022-11-14 10:12:58 +0700

    create update controller for routes category

---
+++ app/category/controller.js
@@ -0,0 +1,62 @@
+const Category = require('./model')
+
+const store = async (req, res, next) => {
+  try {
+    let payload = req.body
+    let category = new Category(payload)
+    await category.save()
+    return res.json(category)
+  } catch (err) {
+    if (err && err.name === 'ValidationError') {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors
+      })
+    }
+
+    next(err)
+  }
+}
+
+const update = async (req, res, next) => {
+  try {
+    let payload = req.body
+    let category = await Category.findByIdAndUpdate(req.params.id, payload, {
+      new: true,
+      runValidators: true
+    })
+    return res.json(category)
+  } catch (err) {
+    if (err && err.name === 'ValidationError') {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors
+      })
+    }
+    next(err)
+  }
+}
+
+const index = async (req, res, next) => {
+  try {
+    let category = await Category.find()
+    return res.json(category)
+  } catch (err) {
+    if (err && err.name === 'ValidationError') {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors
+      })
+    }
+    next(err)
+  }
+}
+
+module.exports = {
+  store,
+  index,
+  update
+}
