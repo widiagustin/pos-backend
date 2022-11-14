45ec298327e9eab3dee9e4bb820fa4cc92607d6e
widiagustin <widhianiagustin14@gmail.com>
2022-11-14 10:39:45 +0700

    create put and delete in routes category

--- app/category/routes.js
+++ app/category/routes.js
@@ -4,6 +4,6 @@ const categoryController = require('./controller')
 router.post('/categories', categoryController.store)
 router.get('/categories', categoryController.index)
 router.put('/categories/:id', categoryController.update)
-
+router.delete('/categories/:id', categoryController.destroy)
 
 module.exports = router
