c75212aef8d02d5017323e467ec3124f232bb3bb
widiagustin <widhianiagustin14@gmail.com>
2022-11-15 12:52:06 +0700

    add routing and authentication for login

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,4 +1,8 @@
 const User = require('../user/model')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
 
 const register = async (req, res, next) => {
   try {
@@ -18,6 +22,39 @@ const register = async (req, res, next) => {
   }
 }
 
+const localStrategy = async (email, password, done) => {
+  try {
+    let user = await User
+      .findOne({ email })
+      .select('-__v -createdAt -updatedAt -cart_items -token')
+    if (!user) return done()
+    if (bcrypt.compareSync(password, user.password)) {
+      ({ password, ...userWithoutPassword } = user.toJSON())
+      return done(null, userWithoutPassword)
+    }
+  } catch (err) {
+    done(err, null)
+  }
+  done()
+}
+
+const login = (req, res, next) => {
+  passport.authenticate('local', async function (err, user) {
+    if (err) return next(err)
+    if (!user) return res.json({ error: 1, message: 'Email or Password incorrect' })
+    let signed = jwt.sign(user, config.secretkey)
+    await User.findByIdAndUpdate(user._id, { $push: { token: signed } })
+
+    res.json({
+      message: 'Login successfull',
+      user,
+      token: signed
+    })
+  })(req, res, next)
+}
+
 module.exports = {
-  register
+  register,
+  localStrategy,
+  login
 }
--- app/auth/routes.js
+++ app/auth/routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const authController = require('./controller')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
+passport.use(new LocalStrategy({ usernameField: 'email' }, authController.localStrategy))
 router.post('/register', authController.register)
+router.post('/login', authController.login)
 
 module.exports = router
