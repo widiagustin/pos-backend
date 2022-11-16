863bfdc595f37f638d956d5a9a89a971658414f9
widiagustin <widhianiagustin14@gmail.com>
2022-11-16 08:19:08 +0700

    add authorization with CASL

--- app/tag/routes.js
+++ app/tag/routes.js
@@ -1,9 +1,19 @@
 const router = require('express').Router()
+const { police_check } = require('../../middlewares')
 const categoryController = require('./controller')
 
-router.post('/tags', categoryController.store)
 router.get('/tags', categoryController.index)
-router.put('/tags/:id', categoryController.update)
-router.delete('/tags/:id', categoryController.destroy)
+router.post('/tags',
+  police_check('create', 'Tag'),
+  categoryController.store
+)
+router.put('/tags/:id',
+  police_check('update', 'Tag'),
+  categoryController.update
+)
+router.delete('/tags/:id',
+  police_check('delete', 'Tag'),
+  categoryController.destroy
+)
 
 module.exports = router
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,4 @@
-const { getToken } = require('../utils')
+const { getToken, policyFor } = require('../utils')
 const jwt = require('jsonwebtoken')
 const config = require('../app/config')
 const User = require('../app/user/model')
@@ -30,6 +30,21 @@ function decodeToken () {
   }
 }
 
+// middleware untuk cek hak akses
+function police_check (action, subject) {
+  return function (req, res, next) {
+    let policy = policyFor(req.user)
+    if (!policy.can(action, subject)) {
+      return res.json({
+        error: 1,
+        message: `You are no allowed to ${action} ${subject}`
+      })
+    }
+    next()
+  }
+}
+
 module.exports = {
-  decodeToken
+  decodeToken,
+  police_check
 }
