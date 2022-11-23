c644862264432e988e32f0a452c3484148959c8f
widiagustin <widhianiagustin14@gmail.com>
2022-11-23 11:20:30 +0700

    change config dotenv

--- app/config.js
+++ app/config.js
@@ -8,8 +8,5 @@ module.exports = {
   secretkey: process.env.SECRET_KEY,
   serviceName: process.env.SERVICE_NAME,
   dbHost: process.env.DB_HOST,
-  dbPort: process.env.DB_PORT,
-  dbUser: process.env.DB_USER,
-  dbPass: process.env.DB_PASS,
-  dbName: process.env.DB_NAME
+  dbKey: process.env.DB_KEY
 }
